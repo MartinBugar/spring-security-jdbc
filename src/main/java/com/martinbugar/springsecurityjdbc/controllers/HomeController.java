ef77d381c633d6d3f683704451a0b55079a622b8
MartinBugar <martin1.bugar@gmail.com>
2020-09-29 09:21:37 +0200

    configurer

---
+++ src/main/java/com/martinbugar/springsecurityjdbc/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.martinbugar.springsecurityjdbc.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home (){
+        return "Welcome user";
+    }
+
+    @GetMapping("/user")
+    public String user (){
+        return "Welcome user";
+    }
+
+
+    @GetMapping("/admin")
+    public String admin (){
+        return "Welcome admin";
+    }
+}
